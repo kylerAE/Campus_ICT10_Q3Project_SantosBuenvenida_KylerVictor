a357d06673fb7ac9a954e7a5c43dbe5355ff97a2
kyy <santos.ky@obmontessori.edu.ph>
2026-02-19 22:44:34 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+# main.py
+
+players = [
+    "Max Ancheta",
+    "Alonso Asuncion",
+    "Enzo Battung",
+    "Vic Buenvenida",
+    "Kayla Casul",
+    "Athena Catapang",
+    "Cade Chua",
+    "Zyan Eusebio",
+    "Radhika Evanghelio",
+    "Mara Fado",
+    "Kleiser Fermocil",
+    "Curt Fernando",
+    "Ethan Francia",
+    "Sophia Jimenez",
+    "Javi Mabilog",
+    "AC Mactal",
+    "Lei Magday",
+    "Yanna Moya",
+    "Zoe Mutia",
+    "Luis Nazareno",
+    "Ara Quinto",
+    "Inigo Romero",
+    "Kyler Santos",
+    "Jaedin Sarao",
+    "Briana Sy",
+    "Charlotte Sy",
+    "Jared Udono",
+    "KC Vida"
+]
+
+print("<ul>")
+for player in players:
+    print("<li>" + player + "</li>")
+print("</ul>")
